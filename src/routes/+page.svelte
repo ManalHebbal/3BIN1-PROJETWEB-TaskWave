bda0f793b97c9ad64d2702eeb359f131a4bd39d5
Manal <manal.hebbal@student.vinci.be>
2024-11-11 16:42:59 +0100

    delete : welkome page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,6 @@
+<!-- +page.svelte dans src/routes/calendar/ -->
 <script>
-    import { goto } from '$app/navigation';
-  </script>
-  
-  <h1>Bienvenue sur le gestionnaire de tâches</h1>
-  <button on:click={() => goto('/calendar')}>Voir le calendrier</button>
-  
+  import Calendar from '../components/Calendar.svelte';
+</script>
+
+<Calendar />
--- src/routes/calendar/+page.svelte
+++
@@ -1,6 +0,0 @@
-<!-- +page.svelte dans src/routes/calendar/ -->
-<script>
-  import Calendar from '../../components/Calendar.svelte';
-</script>
-
-<Calendar />
